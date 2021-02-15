cea0466851bdce576ff726783e571cc8852d38f1
likquietly <45600764+likquietly@users.noreply.github.com>
2021-02-15 20:40:34 +0300

    HW04: Added Project HW.04.Task5

---
+++ HomeWorks/HW.04.Task5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HW._04.Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 90; i > 64; i--)
+            {
+                Console.Write((char)i + " ");
+            }
+        }
+    }
+}
