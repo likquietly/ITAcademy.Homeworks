8efbd61f4d07b84cb76b1717099244823d3a6cc3
likquietly <45600764+likquietly@users.noreply.github.com>
2021-02-15 14:52:55 +0300

    HW04: Added Project HW.04.Task2

---
+++ HomeWorks/HW.04.Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HW._04.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Add();
+        }
+
+        /// <summary>
+        /// Method for adding two numbers, checking for the entered value and result
+        /// </summary>
+        public static void Add()
+        {
+            double firstNumber, secondNumber;
+
+            Console.Write("Enter the first number: ");
+            while (!Double.TryParse(Console.ReadLine(), out firstNumber))
+            {
+                Console.Write("You entered an invalid first value, please, try again: ");
+            }
+
+            Console.Write("Enter the second number: ");
+            while (!Double.TryParse(Console.ReadLine(), out secondNumber))
+            {
+                Console.Write("You entered an invalid second value, please, try again: ");
+            }
+
+            Console.Write("Enter the calculation result: ");
+            double result = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(result == firstNumber + secondNumber ? "correct" : "incorrect");
+
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+        }
+    }
+}
