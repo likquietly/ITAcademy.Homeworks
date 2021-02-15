dd48b8ed74f05af8f24db48138a1b7ca9d765ca6
likquietly <45600764+likquietly@users.noreply.github.com>
2021-02-15 14:19:43 +0300

    HW04: Added Project HW.04.Task1

---
+++ HomeWorks/HW.04.Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HW._04.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Add();
+        }
+
+        public static void Add()
+        {
+            double firstNumber, secondNumber;
+
+            Console.Write("Enter the first number: ");
+            while (!Double.TryParse(Console.ReadLine(), out firstNumber))
+            {
+                Console.Write("You entered an invalid first value, please, try again: ");
+            }
+
+            Console.Write("Enter the second number: ");
+            while (!Double.TryParse(Console.ReadLine(), out secondNumber))
+            {
+                Console.Write("You entered an invalid second value, please, try again: ");
+            }
+
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+        }
+    }
+}
