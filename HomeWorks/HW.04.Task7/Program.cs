64e62802c51649848b1244a8bed15d8d8e75524b
likquietly <45600764+likquietly@users.noreply.github.com>
2021-02-16 11:21:37 +0300

    HW04: Added Project HW.04.Task 7 and Folder Documents

---
+++ HomeWorks/HW.04.Task7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HW._04.Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testIntTry;
+            int testInt = Int32.Parse(Console.ReadLine());
+            Int32.TryParse(Console.ReadLine(), out testIntTry);
+
+            char testCharTry;
+            char testChar = Char.Parse(Console.ReadLine());
+            Char.TryParse(Console.ReadLine(), out testCharTry);
+
+            bool testBoolTry;
+            bool testBool = Boolean.Parse(Console.ReadLine());
+            Boolean.TryParse(Console.ReadLine(), out testBoolTry);
+
+            Console.WriteLine($"{testInt} {testIntTry} {testChar} {testCharTry} {testBool} {testBoolTry}");
+        }
+    }
+}
