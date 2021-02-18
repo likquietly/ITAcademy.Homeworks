ff11048c9a4498af3b6e5dc04d16901844ca08ae
likquietly <45600764+likquietly@users.noreply.github.com>
2021-02-18 17:25:03 +0300

    HW05: Added Project HW.05.Task4

---
+++ HomeWorks/HW.05.Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HW._05.Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string lettersCount = Console.ReadLine();
+            int count = 0;
+
+            Console.WriteLine("Enter the text:");
+            foreach (var item in lettersCount)
+            {
+                //it's 'a' and 'A' of Latin and Cyrillic!
+                if (item == 'a' || item == 'A' || item == 'а' || item == 'А')
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("The numbers of letters 'a' or 'A' in this line: " + count);
+        }
+    }
+}
