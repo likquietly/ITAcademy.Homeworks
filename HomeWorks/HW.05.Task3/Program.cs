e986a074a147720e22643cd98209608311cc1074
likquietly <45600764+likquietly@users.noreply.github.com>
2021-02-18 17:15:44 +0300

    HW05: Added Project HW.05.Task3

---
+++ HomeWorks/HW.05.Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HW._05.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{DateTime.Now.Hour} : {DateTime.Now.Minute} : {DateTime.Now.Second}");
+
+            switch (true)
+            {
+                case true when DateTime.Now.Hour > 8 && DateTime.Now.Hour < 12:
+                    Console.WriteLine("Good morning, guys!");
+                    break;
+                case true when DateTime.Now.Hour > 11 && DateTime.Now.Hour < 15:
+                    Console.WriteLine("Good day, guys!");
+                    break;
+                case true when DateTime.Now.Hour > 14 && DateTime.Now.Hour < 22:
+                    Console.WriteLine("Good evening, guys!");
+                    break;
+                default:
+                    Console.WriteLine("Unsupported range");
+                    break;
+            }
+        }
+    }
+}
