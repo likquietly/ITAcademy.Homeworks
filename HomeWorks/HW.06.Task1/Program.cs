fcbe79115048527b0ff9188c6bb8c8c1286f1e2b
likquietly <45600764+likquietly@users.noreply.github.com>
2021-02-22 10:33:55 +0300

    HW06: Added Project HW.06.Task1

---
+++ HomeWorks/HW.06.Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HW._06.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] randArr = new int[20];
+            int[] userArr = new int[20];
+            int[] summArr = new int[20];
+
+            Random rnd = new Random();
+
+            Console.WriteLine($"Enter {userArr.Length} values for user array");
+
+            //if we know that all three arrays are the same size
+            for (int i = 0; i < userArr.Length; i++)
+            {
+                Console.Write($"{i + 1} element: ");
+                while (!Int32.TryParse(Console.ReadLine(), out userArr[i]))
+                {
+                    Console.Write("You enter the invalid number, please, try again: ");
+                }
+                randArr[i] = rnd.Next(20);
+                summArr[i] = randArr[i] + userArr[i];
+            }
+
+            Console.WriteLine("Rand array: " + string.Join(" ", randArr));
+            Console.WriteLine("User array: " + string.Join(" ", userArr));
+            Console.WriteLine("Summ array: " + string.Join(" ", summArr));
+        }
+    }
+}
