5a232f204bc6c6fdc610b8ec3f5e1c922471a9bd
likquietly <45600764+likquietly@users.noreply.github.com>
2021-02-25 16:29:39 +0300

    HW04: Added Project Hw.04.Task7

---
+++ HomeWorks/HW.07.Task3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace HW._07.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "gdfgdf234dg54gf/23oP42";
+
+            string numbTemp = string.Empty;
+            double number1 = 0.0;
+            char sym = ' ';
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsDigit(text[i]))
+                {
+                    numbTemp += text[i];
+                }
+                else if (text[i] == '+' || text[i] == '-' || text[i] == '*' || text[i] == '/')
+                {
+                    sym = text[i];
+                    number1 = Convert.ToDouble(numbTemp);
+                    numbTemp = string.Empty;
+                }
+            }
+
+            double number2 = Convert.ToDouble(numbTemp);
+
+            switch (sym)
+            {
+                case '+':
+                    Console.WriteLine($"{number1} {sym} {number2} = {number1 + number2}");
+                    break;
+                case '-':
+                    Console.WriteLine($"{number1} {sym} {number2} = {number1 - number2}");
+                    break;
+                case '*':
+                    Console.WriteLine($"{number1} {sym} {number2} = {number1 * number2}");
+                    break;
+                case '/':
+                    Console.WriteLine($"{number1} {sym} {number2} = {number1 / number2}");
+                    break;
+                default:
+                    Console.WriteLine("Inputted an invalid arithmetic symbol");
+                    break;
+            }
+        }
+    }
+}
