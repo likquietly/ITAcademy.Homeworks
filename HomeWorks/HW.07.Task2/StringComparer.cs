cde227282bf22a41bb9eb3ef836f3b9d6a19d4f6
likquietly <45600764+likquietly@users.noreply.github.com>
2021-02-25 14:27:23 +0300

    HW07: Added Project HW.07.Task2

---
+++ HomeWorks/HW.07.Task2/StringComparer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW._07.Task2
+{
+    class StringComparer : IComparer<String>
+    {
+        /// <summary>
+        /// Сomparator for comparing strings by length
+        /// </summary>
+        /// <param name="p1"></param>
+        /// <param name="p2"></param>
+        /// <returns></returns>
+        public int Compare(String p1, String p2)
+        {
+            if (p1.Length > p2.Length)
+                return -1;
+            else if (p1.Length < p2.Length)
+                return 1;
+            else
+                return 0;
+        }
+    }
+}
