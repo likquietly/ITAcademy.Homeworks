e8d9c919defd5a608c24cc385b7367c5d65cf03a
likquietly <45600764+likquietly@users.noreply.github.com>
2021-02-26 15:57:52 +0300

    HW08: Added Project HW.08.Task1

---
+++ HomeWorks/HW.08.Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace HW._08.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("1a!2.3!!.. 4.!.?6 7! ..?");
+
+            bool secondPart = false;
+
+            for (int i = 0; i < sb.Length; i++)
+            {
+                if (sb[i] == '!' || sb[i] == '.' && !secondPart)
+                {
+                    sb.Remove(i, 1);
+                    i--;
+                }
+                else if (sb[i] == '?' || secondPart)
+                {
+                    secondPart = true;
+                    sb.Replace(' ', '_', i, 1);
+                }
+            }
+
+            System.Console.WriteLine("String operation result: \n" + sb.ToString());
+        }
+    }
+}
